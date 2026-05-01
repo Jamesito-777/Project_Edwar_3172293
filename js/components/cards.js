0d01254bb31585e83022d56c060f89f74660c46f
Jamesito-777 <jamesmachao992@gmail.com>
2026-04-30 19:54:43 -0500

    se actualizo repositorio

---
+++ js/components/cards.js
@@ -0,0 +1,33 @@
+
+export async function loadCards(containerSelector, cardIds = []) {
+
+    const container = document.querySelector(containerSelector);
+    if(!container) return;
+
+    try {
+            // Hacemos dos fetch al mismo tiempo, uno para la plantilla de card y otro para los datos 
+        const[templateRes, dataRes] = await Promise.all([
+                fetch("/views/components/cards.html"),
+                fetch("/data/card.json")
+        ])
+
+        const template = await templateRes.text();
+        const cards = await dataRes.json();
+
+    //  Filtro por si se proporciona IDs
+    const filterCards = cardIds.length ? cardIds.filter(card => cardIds.includes(card.id)) : cards; //Si no hay ID las muestra todas
+
+   filterCards.forEach(card => {
+    let html = template
+        .replace("{{id}}", card.id)
+        .replace("{{image}}", card.image)
+        .replace("{{alt}}", card.alt)
+        .replace("{{label}}", card.label);
+
+    container.innerHTML += html;
+});
+
+    }catch(error){
+        console.log("Error cargando las cards", error)
+    }
+}
